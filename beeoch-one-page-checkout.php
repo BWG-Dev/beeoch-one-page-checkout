6ac4cca3789e4aae146cbc068997a7fa4de28f10
Yariel Gordillo <yariel.gordillo@gmail.com>
2026-08-14 09:51:18 -0400

    Whitelabeling the plugin

--- beeoch-one-page-checkout.php
+++ beeoch-one-page-checkout.php
@@ -5,7 +5,7 @@
  * Version:           1.25.1
  * Requires PHP:      8.1
  * Requires at least: 6.5
- * Author:            Bee-Och Organics
+ * Author:            Web Bennet Group
  * Text Domain:       beeoch-opc
  *
  * @package Beeoch\OPC
